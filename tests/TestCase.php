ebeea92d0eecde6f4bd8ac657b840af2274902bc
Danu-Nur <diramadhan13@gmail.com>
2026-09-14 13:47:33 +0700

    test: configure isolated SQLite database for self-contained automated testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,9 +8,16 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
+    protected static $migrated = false;
+
     protected function setUp(): void
     {
         parent::setUp();
+
+        if (config('database.default') === 'sqlite' && !static::$migrated) {
+            \Illuminate\Support\Facades\Artisan::call('migrate');
+            static::$migrated = true;
+        }
 
         $this->withoutMiddleware([
             \App\Http\Middleware\VerifyCsrfToken::class,
